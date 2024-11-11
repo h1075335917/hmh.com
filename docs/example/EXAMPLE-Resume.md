ddaa6e2b442cf9eea4bab23516b241cd85c50c54
heminhua <1075335917@qq.com>
2024-11-11 22:37:01 +0800

    feat：Update

--- docs/example/EXAMPLE-Resume.md
+++
@@ -1,7 +0,0 @@
-# 个人信息
-
-```mdx-code-block
-import Resume from '@site/src/components/Resume';
-
-<Resume />
-```
--- sidebars.ts
+++ sidebars.ts
@@ -26,7 +26,6 @@ const sidebars: SidebarsConfig = {
   // But you can create a sidebar manually
   example: [
     'example/EXAMPLE-Video',
-    'example/EXAMPLE-Resume',
     {
       type: 'category',
       label: 'DocCardList示例',
