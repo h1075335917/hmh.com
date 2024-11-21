a6d1c9aa426e2f3e34dd59955400bf17be27fb7d
mhuahe <mhuahe>
2024-11-21 17:11:47 +0800

    feat：medium-zoom实现图片缩放功能

--- src/theme/MDXComponents/Img/index.tsx
+++ src/theme/MDXComponents/Img/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import clsx from 'clsx';
+import mediumZoom from 'medium-zoom';
 import type {Props} from '@theme/MDXComponents/Img';
 
 import styles from './styles.module.css';
@@ -9,9 +10,21 @@ function transformImgClassName(className?: string): string {
 }
 
 export default function MDXImg(props: Props): JSX.Element {
+  const imgRef = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    if (imgRef.current) {
+      mediumZoom(imgRef.current, {
+        background: 'rgba(0, 0, 0, 0.5)',
+        margin: 24,
+      });
+    }
+  }, []);
+
   return (
     // eslint-disable-next-line jsx-a11y/alt-text
     <img
+      ref={imgRef}
       decoding="async"
       loading="lazy"
       {...props}
